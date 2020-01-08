9ada446d81a6b4e6398d06a60b49ed5ed0cc3b98
tcbthai <baothai30491@gmail.com>
2020-01-07 21:06:06 -0800

    Add files via upload

---
+++ precache-manifest.77f382b0c64cd6e17fd89eebaa633895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9ebbc60280e289b7d2579c6cb5f019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "473d2f19d5cde829d883",
+    "url": "/static/css/main.cc750bf7.chunk.css"
+  },
+  {
+    "revision": "8795d4af043ddfe37042",
+    "url": "/static/js/2.7cb9f535.chunk.js"
+  },
+  {
+    "revision": "473d2f19d5cde829d883",
+    "url": "/static/js/main.0664c964.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d0993b472ae0b77405045bf0ccf956e",
+    "url": "/static/media/background.0d0993b4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
